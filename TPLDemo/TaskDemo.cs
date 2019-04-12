e9b1f6abb2ecdeee44b6ec0e833d689c1c754fe8
Leon <leon.id@qq.com>
2019-04-12 11:06:34 +0800

    AsyncState

--- TPLDemo/TaskDemo.cs
+++ TPLDemo/TaskDemo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Emit;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TPLDemo
@@ -12,7 +13,7 @@ namespace TPLDemo
             // Task 将在异步线程运行
             Task task = new Task(() => Helper.Print("Greet from Task."));
             task.Start();
-            
+
             Helper.Print("Hello from Main.");
 
             // 确保 Task 完成之后再退出控制台
@@ -24,7 +25,17 @@ namespace TPLDemo
             }
             Helper.PrintSplit();
 
-            Task.Factory.StartNew(() => Helper.Print("Greet from Task.Factory."));
+            Task.Factory.StartNew(() => Helper.Print("Greet from Task.Factory.")).Wait();
+            Helper.PrintSplit();
+
+            Task[] tasks = models.Take(10).Select(model => Task.Factory.StartNew(
+                    (m) => Helper.Print($"Processing {(m as RunModel).Name}"),
+                    model))
+                .ToArray();
+            // 等待全部 Task 完成
+            Task.WaitAll(tasks);
+            // 回到主线程输出 Task State
+            Array.ForEach(tasks, (t) => Helper.Print($"Task-{t.Id} Processed {(t.AsyncState as RunModel).Name}"));
         }
     }
 }
