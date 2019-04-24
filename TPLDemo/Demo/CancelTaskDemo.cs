4a7ec4ffd9560f9a5984f92a90de017c94bf50f9
Leon <leon.id@qq.com>
2019-04-24 14:59:46 +0800

    完善任务取消演示；

--- TPLDemo/Demo/CancelTaskDemo.cs
+++ TPLDemo/Demo/CancelTaskDemo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TPLDemo.Model;
 
@@ -13,7 +15,49 @@ namespace TPLDemo.Demo
     {
         public override void Run()
         {
-            
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+            token.Register(() => { Helper.PrintLine($"取消了 Token"); });
+            source.CancelAfter(250);
+
+            Task task = Task.Factory.StartNew(
+                () =>
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(100);
+                        Helper.PrintLine("检查取消状态...");
+                        /* 如果 Token 已经请求取消了，则抛出异常
+                         * 这种方式会使 Task 状态置为 Canceled；
+                         */
+                        // token.ThrowIfCancellationRequested();
+
+                        /* 或者温和的方式，悄悄return；
+                         * 这种方式会使 Task 状态置为 RanToCompletion；
+                         */
+                        if (token.IsCancellationRequested)
+                        {
+                            Helper.PrintLine($"悄悄 return");
+                            // 这里还可以悄悄处理些事情
+                            return;
+                        }
+                    }
+                },
+                token);
+
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException e1) when (!(e1.InnerException is TaskCanceledException))
+            {
+                // 线程內异常将以 AggregateException 传出，同 task.Exception
+                Helper.PrintLine($"AggregateException：{string.Join("；", e1.InnerExceptions.Select(ex => ex.Message))}");
+            }
+            finally
+            {
+                Helper.PrintLine($"任务已经取消");
+            }
         }
     }
 }
--- TPLDemo/Demo/DemoFactory.cs
+++ TPLDemo/Demo/DemoFactory.cs
@@ -16,8 +16,8 @@ namespace TPLDemo.Demo
             { "wait", new Lazy<IRunableDemo>(() =>  new WaitTaskDemo()) },
             { "compose", new Lazy<IRunableDemo>(() =>  new ComposeTaskDemo()) },
             { "exception", new Lazy<IRunableDemo>(() =>  new ExceptionDemo()) },
-            { "cancel", new Lazy<IRunableDemo>(() =>  new CancelTaskDemo()) },
             { "factory", new Lazy<IRunableDemo>(() =>  new TaskFactoryDemo()) },
+            { "cancel", new Lazy<IRunableDemo>(() =>  new CancelTaskDemo()) },
         };
 
         /// <summary>
