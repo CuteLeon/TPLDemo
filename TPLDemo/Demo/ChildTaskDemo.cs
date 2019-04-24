71f2944c473c11edeb008353f8f1addfa5b6f517
Leon <leon.id@qq.com>
2019-04-24 17:26:36 +0800

    TaskCreationOptions.DenyChildAttach 声明禁止附加子任务；

--- TPLDemo/Demo/ChildTaskDemo.cs
+++ TPLDemo/Demo/ChildTaskDemo.cs
@@ -13,19 +13,21 @@ namespace TPLDemo.Demo
             // 默认情况下，子任务和父任务没有在同一线程，父进程不会等待子进程而直接结束
             // 父任务可以通过 TaskCreationOptions.DenyChildAttach 声明禁止附加子任务。
             Task childTask = null;
-            var parentTask_1 = Task.Factory.StartNew(() =>
-            {
-                Helper.PrintLine("开始执行父任务-1...");
+            var parentTask_1 = Task.Factory.StartNew(
+                () =>
+                {
+                    Helper.PrintLine("开始执行父任务-1...");
 
-                childTask = Task.Factory.StartNew(() =>
-                      {
-                          Helper.PrintLine("开始执行子任务-1...");
-                          Task.Delay(10).Wait();
-                          Helper.PrintLine("子任务-1完成...");
-                      });
-                // 如果子任务返回 Result，则在父任务内获取子任务的 Result 可以间接地实现父任务等待子任务
-                Helper.PrintLine("父任务-1完成...");
-            });
+                    childTask = Task.Factory.StartNew(() =>
+                          {
+                              Helper.PrintLine("开始执行子任务-1...");
+                              Task.Delay(10).Wait();
+                              Helper.PrintLine("子任务-1完成...");
+                          });
+                    // 如果子任务返回 Result，则在父任务内获取子任务的 Result 可以间接地实现父任务等待子任务
+                    Helper.PrintLine("父任务-1完成...");
+                },
+                TaskCreationOptions.DenyChildAttach);
 
             // 需要同时手动等待父任务和子任务结束
             parentTask_1.Wait();
@@ -44,6 +46,7 @@ namespace TPLDemo.Demo
                     },
                     TaskCreationOptions.AttachedToParent);
             });
+            Helper.PrintSplit();
 
             // 这里只需要手动等待父任务结束，而不用手动等待子任务结束，因为父任务会隐式等待子任务
             parentTask_2.Wait();
