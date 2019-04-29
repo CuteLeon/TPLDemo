9cc1371b27ba837bf0e4b00a98fe2cdf32fc8e91
Leon <leon.id@qq.com>
2019-04-29 16:46:24 +0800

    async 与 await 同时使用以避免线程阻塞；

--- TPLDemo/Demo/TaskDemos/TaskDemo.cs
+++ TPLDemo/Demo/TaskDemos/TaskDemo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using TPLDemo.Model;
 
@@ -50,6 +51,54 @@ namespace TPLDemo.Demo.TaskDemos
             Helper.PrintLine($"Task.Result = {funcTask.Result}");
             funcTask = Task.Factory.StartNew<RunModel>(() => new RunModel() { Name = "I'm Result-2." });
             Helper.PrintLine($"Task.Result = {funcTask.Result}");
+            Helper.PrintSplit();
+
+            // await
+            Helper.PrintLine($"async 需要和 await 同时使用才可以达到异步效果");
+            Helper.PrintLine($"请求执行一个复杂的功能...");
+            task = this.ComplexFunction(models.FirstOrDefault());
+            Helper.PrintLine($"不希望等待复杂的功能，而继续运行...");
+
+            task.Wait();
+        }
+
+        /// <summary>
+        /// 在新线程执行的方法
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private async Task ComplexFunction(RunModel model)
+        {
+            Helper.PrintLine($"执行一个复杂的功能：参数={model.Name}");
+
+            // 需要使用 await 而不是 Task.Result 以避免 Run() 阻塞
+            int result;
+
+            result = await this.ComplexCalculationTask(model);
+            Helper.PrintLine($"得到结果-1：{result}");
+
+            // 对于非 Task<> 类型的返回值，需要使用 Task.Run 包装下，
+            result = await Task.Run(() => this.ComplexCalculation(model));
+            Helper.PrintLine($"得到结果-2：{result}");
+        }
+
+        private Task<int> ComplexCalculationTask(RunModel model)
+        {
+            Helper.PrintLine($"开始一次复杂的计算：参数={model.Name}");
+            // 需要使用 Task 而不是直接 return 以避免 Run() 阻塞
+            return Task.Factory.StartNew(() =>
+            {
+                Helper.PrintLine($"Task.Factory.StartNew：参数={model.Name}");
+                Thread.Sleep(100);
+                return Environment.TickCount;
+            });
+        }
+
+        private int ComplexCalculation(RunModel model)
+        {
+            Helper.PrintLine($"Native invoke：参数={model.Name}");
+            Thread.Sleep(100);
+            return Environment.TickCount;
         }
     }
 }
